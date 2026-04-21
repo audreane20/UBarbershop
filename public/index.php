338b53a24da71b5202b0143871beb6adaa483164
FORTIN Victor <victor.fortin078@gmail.com>
2026-04-21 09:50:52 -0400

    soon finished twig convertion

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
@@ -12,21 +16,68 @@ $twig = Twig::create(__DIR__ . '/../templates', [
     'cache' => false, // Mettre un chemin (ex: '/tmp/cache') en production
 ]);
 
-$app->add(TwigMiddleware::create($app, $twig));
-
 $app->setBasePath('/UBarbershop/public');
+
+// Ajouter base_path à tous les templates Twig
+$app->addRoutingMiddleware();
+
+$app->add(function (Request $request, RequestHandler $handler) {
+    $twig = Twig::fromRequest($request);
+    $twig->getEnvironment()->addGlobal('base_path', '/UBarbershop/public');
+    return $handler->handle($request);
+});
+
+$app->add(TwigMiddleware::create($app, $twig));
 
 $app->addErrorMiddleware(true, true, true);
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
 
 $app->get('/', function (Request $request, Response $response) {
     $view = Twig::fromRequest($request);
 
-    return $view->render($response, 'index.html.twig'); 
+    return $view->render($response, 'index.html.twig', [
+        'base_path' => '/UBarbershop/public'
+    ]); 
     });
     
 
+$app->get('/about', function (Request $request, Response $response) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'about.html.twig', [
+        'base_path' => '/UBarbershop/public'
+    ]); 
+});
+
+$app->get('/contact', function (Request $request, Response $response) {
+    $view = Twig::fromRequest($request);
+
+     return $view->render($response, 'contact.html.twig');
+});
+
+$app->get('/login', function (Request $request, Response $response) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'login.html.twig');
+});
+
+$app->get('/appointment', function (Request $request, Response $response) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'appointment.html.twig');
+});
+
+$app->get('/callendar', function (Request $request, Response $response) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'callendar.html.twig');
+});
+
+$app->get('/register', function (Request $request, Response $response) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'register.html.twig');
+});
 
 $app->run();
